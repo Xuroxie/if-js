9e42ecf28ff525b16010f8188e96d44c9c259816
Xuroxie <71769390+Xuroxie@users.noreply.github.com>
2020-11-30 14:36:00 +0300

    Add files via upload
    Я не смогла заменить 0 на строку

--- main.js
+++ main.js
@@ -1,44 +1,30 @@
-let user = "John Doe";
-
-console.log (user);
-
-let student = "Ksenia";
-
-console.log (student);
-
-user = student;
-
-console.log (user); // в консоле будет выведено значение student
-
-test = 1;
-test++;
-test = test + "1"; //21
-
-console.log (test);
-
-test--; // 20
-
-console.log (test);
-
-let result = 1;
-let arr = [2, 3, 5, 8];
-
-for (let i = 0; i < arr.length; i++) {
-    result = result * arr[i];
+//1
+function isPalindrome(str) {
+    let strLen = str.length,
+        strReverse = str.split('').reverse().join('');
+    if (strReverse == str) {
+        return 'yes';
+    } else {
+        return 'no';
+    }
 }
+test = isPalindrome('кот');
+test2 = isPalindrome('шалаш');
 
-console.log(result);
+console.log(test);
+console.log(test2);
 
-arr = [2, 5, 8, 15, 0, 6, 20, 3];
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i]>5 && arr[i]<10) {
-        console.log (arr[i]);
+//2
+function min(a, b) {
+    if (a < b) {
+        return a;
+    } else
+    {
+        return b;
     }
 }
 
-arr = [2, 5, 8, 15, 0, 6, 20, 3];
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 == 0 && arr[i] !=0 ) {
-        console.log (arr[i]);
-    }
+//3
+function max (a, b) {
+    return a > b ? a : b;
 }
